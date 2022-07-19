a55f4f056af16e732250210b1fa7215efa801c12
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-07-19 10:50:09 +0630

    Matching Patterns for the win/loose

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Square from './components/Square';
+import { Patterns } from './Patterns';
 
 function App() {
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState("X")
+  const [result, setResult] = useState({ winner: "none", state: "none" });
+
+  useEffect(() => {
+    checkWin();
+    
+  }, [board]);
+
+  useEffect(() => {
+    if (result.state != "none") {
+      alert(`Game Finished! Winning Player: ${result.winner}`);
+    }
+  }, [result]);
 
   const chooseSquare = (square) => {
       setBoard(
@@ -22,6 +35,24 @@ function App() {
         setPlayer("X")
       }
   }
+
+  const checkWin = () => {
+    Patterns.forEach((currPattern) => {
+      const firstPlayer = board[currPattern[0]];
+      if (firstPlayer == "") return;
+      let foundWinningPattern = true;
+      currPattern.forEach((idx) => {
+        if (board[idx] != firstPlayer) {
+          foundWinningPattern = false;
+        }
+      });
+
+      if (foundWinningPattern) {
+        setResult({ winner: player, state: "Won" });
+      }
+    });
+  };
+
   return (
     <div className='App'>
         <div className='board'> 
